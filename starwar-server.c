5a2a70e5214026794cde1a095596067a2df2585f
Adam Bławat <blawat.adam@gmail.com>
2014-05-23 23:44:19 +0200

    Dodałem detekcję i obsługę zamknięcia gniazda ze strony gracza.

--- starwar-server.c
+++ starwar-server.c
@@ -16,9 +16,9 @@ int main()
 {
     const char server_name[128] = "starwar-test-server";
     
-    int socket1;
-    int socket2;
-    int socket3;
+    int socket_listen;
+    int socket_client1;
+    int socket_client2;
     
     char buffer[BUFFER_LEN];
     double position[2];
@@ -37,60 +37,75 @@ int main()
     strcpy(server_addres.sun_path, server_name);
     
     
-    socket1 = socket(AF_UNIX, SOCK_STREAM, 0);
+    socket_listen = socket(AF_UNIX, SOCK_STREAM, 0);
     
-    bind(socket1, (struct sockaddr *)&server_addres, offsetof(struct sockaddr_un, sun_path) + strlen(server_addres.sun_path));
+    bind(socket_listen, (struct sockaddr *)&server_addres, offsetof(struct sockaddr_un, sun_path) + strlen(server_addres.sun_path));
     
-    listen(socket1, 10);
+    listen(socket_listen, 10);
     
     addres_len = sizeof(struct sockaddr_un);
     
     puts("Waiting for connection...");
-    socket2 = accept(socket1, (struct sockaddr *)&client_addres1, &addres_len);
+    socket_client1 = accept(socket_listen, (struct sockaddr *)&client_addres1, &addres_len);
     puts("Client 1 connected.");
     
     puts("Waiting for connection...");
-    socket3 = accept(socket1, (struct sockaddr *)&client_addres2, &addres_len);
+    socket_client2 = accept(socket_listen, (struct sockaddr *)&client_addres2, &addres_len);
     puts("Client 2 connected.");
     
     int blocking = 1;
     
-    ioctl(socket2, FIONBIO, &blocking);
-    ioctl(socket3, FIONBIO, &blocking);
+    ioctl(socket_client1, FIONBIO, &blocking);
+    ioctl(socket_client2, FIONBIO, &blocking);
     
     while (1)
     {
-        result = recv(socket2, (char *)&player_move, BUFFER_LEN, 0);
+        result = recv(socket_client1, (char *)&player_move, BUFFER_LEN, 0);
         
         if (result == sizeof(Movement))
         {
             player_move.player_id = 1;
-            send(socket3, (char *)&player_move, sizeof(Movement), 0);
+            send(socket_client2, (char *)&player_move, sizeof(Movement), 0);
             
             puts("recv 1");
             fflush(stdout);
         }
+        else if (result == 0)
+        {
+            close(socket_client1);
+            close(socket_client2);
+            
+            puts("Client 1 disconnected.");
+            break;
+        }
         
-        //usleep(2000);
+        //...
         
-        result = recv(socket3, (char *)&player_move, sizeof(Movement), 0);
+        result = recv(socket_client2, (char *)&player_move, sizeof(Movement), 0);
         
         if (result == sizeof(Movement))
         {
             player_move.player_id = 2;
-            send(socket2, (char *)&player_move, sizeof(Movement), 0);
+            send(socket_client1, (char *)&player_move, sizeof(Movement), 0);
             
             puts("recv 2");
             fflush(stdout);
         }
+        else if (result == 0)
+        {
+            close(socket_client1);
+            close(socket_client2);
+            
+            puts("Client 2 disconnected.");
+            break;
+        }
         
-        usleep(500);
+        usleep(100);
     }
     
     puts("Connection closed.");
     
-    close(socket2);
-    close(socket1);
+    close(socket_listen);
     unlink(server_name);
     
     return 0;
