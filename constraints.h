0e400f98c7b2330219ec7e2ededfa73b7abd2be6
Adam Bławat <blawat.adam@gmail.com>
2014-05-23 20:12:18 +0200

    Dodałem zbiór predefinowanych stałych.

---
+++ constraints.h
@@ -0,0 +1,17 @@
+#ifndef CONSTRAINTS_H
+#define CONSTRAINTS_H
+
+#define MLD 1000000000.0
+
+#define BUFFER_LEN     64
+#define UNIX_PATH_MAX  128
+
+#define WINDOW_X       640
+#define WINDOW_Y       480
+
+#define AREA_WIDTH     40.0
+#define AREA_HEIGHT    30.0
+
+#define MOVE_SPEED     8.0
+
+#endif
