6c2646103f815aa5996d7623b330ed5b82f8f644
Adam Bławat <blawat.adam@gmail.com>
2014-05-24 17:32:26 +0200

    Dodałem odbieranie przeciwników z serwera oraz rysowanie ich.

--- starwar-client.c
+++ starwar-client.c
@@ -12,6 +12,25 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 
+typedef struct player
+{
+    double position_x;
+    double position_y;
+    
+    unsigned int color_index;
+    unsigned int score;
+}
+Player;
+
+typedef struct enemy
+{
+    double position_x;
+    double position_y;
+    
+    double moving_speed;
+}
+Enemy;
+
 #include "constraints.h"
 #include "comunication.h"
 
@@ -36,12 +55,14 @@ void create_window(Display **display, Window *window, Pixmap *pixmap)
     *pixmap = XCreatePixmap(*display, *window, WINDOW_X, WINDOW_Y, 24);
 }
 
-void colors_alloc(Display **display, XColor *player_color)
+void colors_alloc(Display **display, XColor *colors)
 {
     Colormap colormap = DefaultColormap(*display, DefaultScreen(*display));
     XColor dummy;
     
-    XAllocNamedColor(*display, colormap, "cyan", player_color, &dummy);
+    XAllocNamedColor(*display, colormap, "dodger blue", &colors[0], &dummy);
+    XAllocNamedColor(*display, colormap, "dark orange", &colors[1], &dummy);
+    XAllocNamedColor(*display, colormap, "sienna", &colors[2], &dummy);
 }
 
 int main()
@@ -53,21 +74,29 @@ int main()
     int n_events;
     int send_to_server;
     
+    int my_id;
+    
     struct timespec ts1;
     struct timespec ts2;
     
     double d_time;
     
-    char buffer[BUFFER_LEN];
-    double position[2];
-    double my_position[2];
+    char   *comunication_buffer = malloc(BUFFER_LEN);
+    size_t  comunication_len;
     
-    Movement player_move;
+    Enemy *enemies = malloc(sizeof(Enemy) * 2);
+    unsigned int n_enemies;
+    
+    Player players[2];
+    
+    Movement  object_movement;
+    Enemies  *all_enemies;
     
     int is_me_moveing;
     int is_player_moveing;
     
     int is_running;
+    int my_move_direction;
     
     const int step_x = WINDOW_X / AREA_WIDTH;
     const int step_y = WINDOW_Y / AREA_HEIGHT;
@@ -85,12 +114,11 @@ int main()
     Pixmap   pixmap;
     
     XEvent   event;
-    XColor   player_color;
+    XColor   colors[3];
     
     
     server_addres.sun_family = AF_UNIX;
     strcpy(server_addres.sun_path, server_name);
-    
     
     client_socket = socket(AF_UNIX, SOCK_STREAM, 0);
     
@@ -102,24 +130,36 @@ int main()
     {
         puts("Connection to server established.");
         
+        recv(client_socket, (char *)&my_id, sizeof(int), 0);
+        
+        recv(client_socket, comunication_buffer, BUFFER_LEN, 0);
+        
+        all_enemies = (Enemies *)comunication_buffer;
+        n_enemies = all_enemies -> size;
+        comunication_len = sizeof(Enemy) * n_enemies;
+        memcpy(enemies, &(all_enemies -> enemies), comunication_len);
+        
+        players[0].position_x = 20.0;
+        players[0].position_y = 20.0;
+        players[0].color_index = 0;
+        players[1].position_x = 20.0;
+        players[1].position_y = 20.0;
+        players[1].color_index = 1;
+        
         int blocking = 1;
         
         ioctl(client_socket, FIONBIO, &blocking);
-        
-        position[0] = 20.0;
-        position[1] = 20.0;
-        
-        my_position[0] = 20.0;
-        my_position[1] = 20.0;
         
         is_me_moveing = 0;
         is_player_moveing = 0;
         
         create_window(&display, &window, &pixmap);
-        colors_alloc(&display, &player_color);
+        colors_alloc(&display, colors);
         
         player_key = 0;
         send_to_server = 0;
+        
+        my_move_direction = 0;
         
         is_running = 1;
         
@@ -127,11 +167,11 @@ int main()
         {
             clock_gettime(CLOCK_REALTIME, &ts1);
             
-            result = recv(client_socket, (char *)&player_move, sizeof(Movement), 0);
+            result = recv(client_socket, (char *)&object_movement, sizeof(Movement), 0);
             
             if (result == sizeof(Movement))
             {
-                if (player_move.direction != 0)
+                if (object_movement.direction != 0)
                     is_player_moveing = 1;
                 else
                     is_player_moveing = 0;
@@ -139,18 +179,36 @@ int main()
             
             if (is_player_moveing)
             {
-                switch (player_move.direction)
+                switch (object_movement.direction)
                 {
-                    case 1: position[1] -= MOVE_SPEED * d_time;
+                    case 1: players[object_movement.object_id].position_y -= MOVE_SPEED * d_time;
                             break;
                             
-                    case 2: position[1] += MOVE_SPEED * d_time;
+                    case 2: players[object_movement.object_id].position_y += MOVE_SPEED * d_time;
                             break;
                             
-                    case 3: position[0] -= MOVE_SPEED * d_time;
+                    case 3: players[object_movement.object_id].position_x -= MOVE_SPEED * d_time;
                             break;
                             
-                    case 4: position[0] += MOVE_SPEED * d_time;
+                    case 4: players[object_movement.object_id].position_x += MOVE_SPEED * d_time;
+                            break;
+                }
+            }
+            
+            if (is_me_moveing)
+            {
+                switch (my_move_direction)
+                {
+                    case 1: players[my_id].position_y -= MOVE_SPEED * d_time;
+                            break;
+                            
+                    case 2: players[my_id].position_y += MOVE_SPEED * d_time;
+                            break;
+                            
+                    case 3: players[my_id].position_x -= MOVE_SPEED * d_time;
+                            break;
+                            
+                    case 4: players[my_id].position_x += MOVE_SPEED * d_time;
                             break;
                 }
             }
@@ -160,9 +218,17 @@ int main()
             XSetForeground(display, gc, BlackPixel(display, DefaultScreen(display)));
             XFillRectangle(display, pixmap, gc, 0, 0, WINDOW_X, WINDOW_Y);
             
-            XSetForeground(display, gc, player_color.pixel);
-            XFillRectangle(display, pixmap, gc, (int)(step_x * my_position[0]) - 10, (int)(step_y * my_position[1]) - 10, 20, 20);
-            XFillRectangle(display, pixmap, gc, (int)(step_x * position[0]) - 10, (int)(step_y * position[1]) - 10, 20, 20);
+            XSetForeground(display, gc, colors[0].pixel);
+            XFillRectangle(display, pixmap, gc, (int)(step_x * players[0].position_x) - 10, (int)(step_y * players[0].position_y) - 10, 20, 20);
+            XSetForeground(display, gc, colors[1].pixel);
+            XFillRectangle(display, pixmap, gc, (int)(step_x * players[1].position_x) - 10, (int)(step_y * players[1].position_y) - 10, 20, 20);
+            
+            XSetForeground(display, gc, colors[2].pixel);
+            
+            for (i = 0; i < n_enemies; i++)
+            {
+                XFillRectangle(display, pixmap, gc, (int)(step_x * enemies[i].position_x) - 10, (int)(step_y * enemies[i].position_y) - 10, 20, 20);
+            }
             
             XCopyArea(display, pixmap, window, gc, 0, 0, WINDOW_X, WINDOW_Y, 0, 0);
             XFlush(display);
@@ -173,8 +239,8 @@ int main()
             
             for (i = n_events; i > 4; i--)
             {
-                printf("%d\n", n_events);
-                fflush(stdout);
+                //printf("%d\n", n_events);
+                //fflush(stdout);
                 
                 XNextEvent(display, &event);
                 
@@ -195,8 +261,6 @@ int main()
                             player_key = 1;
                             send_to_server = 1;
                             
-                            my_position[1] -= MOVE_SPEED * d_time;
-                            
                             puts("Key Up pressed.");
                             fflush(stdout);
                         }
@@ -204,8 +268,6 @@ int main()
                         {
                             player_key = 2;
                             send_to_server = 1;
-                            
-                            my_position[1] += MOVE_SPEED * d_time;
                             
                             puts("Key Down pressed.");
                             fflush(stdout);
@@ -215,8 +277,6 @@ int main()
                             player_key = 3;
                             send_to_server = 1;
                             
-                            my_position[0] -= MOVE_SPEED * d_time;
-                            
                             puts("Key Left pressed.");
                             fflush(stdout);
                         }
@@ -225,8 +285,6 @@ int main()
                             player_key = 4;
                             send_to_server = 1;
                             
-                            my_position[0] += MOVE_SPEED * d_time;
-                            
                             puts("Key Right pressed.");
                             fflush(stdout);
                         }
@@ -234,10 +292,13 @@ int main()
                     
                     if (send_to_server)
                     {
-                        player_move.player_id = 0;
-                        player_move.direction = player_key;
+                        my_move_direction = player_key;
+                        is_me_moveing = 1;
                         
-                        send(client_socket, (char *)&player_move, sizeof(Movement), 0);
+                        object_movement.object_id = my_id;
+                        object_movement.direction = player_key;
+                        
+                        send(client_socket, (char *)&object_movement, sizeof(Movement), 0);
                         send_to_server = 0;
                     }
                 }
@@ -296,10 +357,13 @@ int main()
                         
                         if (send_to_server)
                         {
-                            player_move.player_id = 0;
-                            player_move.direction = 0;
+                            my_move_direction = 0;
+                            is_me_moveing = 0;
                             
-                            send(client_socket, (char *)&player_move, sizeof(Movement), 0);
+                            object_movement.object_id = my_id;
+                            object_movement.direction = 0;
+                            
+                            send(client_socket, (char *)&object_movement, sizeof(Movement), 0);
                             send_to_server = 0;
                         }
                     }
